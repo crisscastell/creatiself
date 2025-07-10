dc9f2e1e9b89b89b798939364bd908b09e6ee6b0
CrisCastell <cristiamcastell.dev@gmail.com>
2025-07-09 21:44:02 -0300

    Fixed error databases

--- project/settings.py
+++ project/settings.py
@@ -91,6 +91,16 @@ WSGI_APPLICATION = 'project.wsgi.application'
 #     }
 # }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
